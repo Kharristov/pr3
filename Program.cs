c4edc2bf0e773e7a90077611f7c288a801173a21
Kharristov <annawey486@gmail.com>
2024-12-05 15:27:42 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static int DigitalRoot(int num)
+    {
+        while (num >= 10)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10; 
+                num /= 10; 
+            }
+            num = sum;  
+        }
+
+        return num; 
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число для вычисления цифрового корня:");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Цифровой корень: {DigitalRoot(num)}");
+    }
+}
